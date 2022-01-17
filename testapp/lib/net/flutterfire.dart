2e7a1f443f2d741db278d7c7297001f7e2b24c88
“sashininliyanage” <nimthara.liyanage@gmail.com>
2022-01-17 18:14:49 +0530

    authentication ui

---
+++ testapp/lib/net/flutterfire.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> signIn(String email, String password) async {}
+
+Future<void> register(String email, String password) async {
+  try {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provide is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e.toString());
+  }
+}
